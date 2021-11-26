ed8438c45d74ad68d60be7d25c13e97950e22d99
Sei <seitaku@wethink.com.tw>
2021-11-26 14:20:38 +0800

    aop配置

---
+++ src/main/java/com/learn/cache/springbootredisexample/model/aop/LogAspect.java
@@ -0,0 +1,12 @@
+package com.learn.cache.springbootredisexample.model.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LogAspect {
+
+
+}
